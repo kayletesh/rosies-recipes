068a5771b79fa331aefd2c7a1421c6d95ee520bf
kayletesh <kayletesh@gmail.com>
2024-08-24 14:38:55 -0400

    Add text to recipe cards. Add images to cards. Fix styling for recipe card text. Fix sizing issue with imgs. Add function to toggle about button text when clicked.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// alert("HELLO WORLD!!");
+//buttons
+
+const aboutButton = document.querySelector("#about-button");
+
+aboutButton.addEventListener("click", () => {
+  toggleAboutTab();
+});
+
+function toggleAboutTab() {
+  console.log("about tab toggled");
+  if (aboutButton.innerText === "About Rosie's Recipes") {
+    aboutButton.innerText = "Back to Recipes";
+  } else {
+    aboutButton.innerText = "About Rosie's Recipes";
+  }
+}
